6afd07fd022d49211a45f5c408704303a8ba7c97
mikera <mike@mikera.net>
2025-06-02 19:02:05 +0100

    Asset prefix test

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
-  assetPrefix: process.env.PAGES_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+    ? `${process.env.NEXT_PUBLIC_BASE_PATH}/`
+    : '',
   trailingSlash: true,
   images: {
     unoptimized: true,
