095183cc9c5cb808847cca06781e361761e483b0
mikera <mike@mikera.net>
2025-06-02 19:03:54 +0100

    Another images test

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
-    ? `${process.env.NEXT_PUBLIC_BASE_PATH}/`
+  assetPrefix: process.env.PAGES_BASE_PATH
+    ? `${process.env.PAGES_BASE_PATH}/`
     : '',
   trailingSlash: true,
   images: {
