a09dd49ef251c36b265b3bc30c8728e11c61542f
mikera <mike@mikera.net>
2025-06-02 19:27:45 +0100

    More image tests

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,12 @@ import type { NextConfig } from 'next';
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.PAGES_BASE_PATH,
+  basePath: process.env.PAGES_BASE_PATH
+  ? `/${process.env.PAGES_BASE_PATH}`
+  : '',
   assetPrefix: process.env.PAGES_BASE_PATH
-    ? `${process.env.PAGES_BASE_PATH}/`
-    : '',
+    ? `/${process.env.PAGES_BASE_PATH}/`
+    : '/',
   trailingSlash: true,
   images: {
     unoptimized: true,
