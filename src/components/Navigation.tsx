e52cd02adf2e6da9f9068a8c9e5f9878d43e4f9f
mikera <mike@mikera.net>
2025-10-16 12:04:28 +0100

    More styling

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,28 +6,28 @@ export default function Navigation() {
     <header className="py-6">
       <nav className="flex justify-between items-center">
         <div className="flex items-center space-x-8">
-          <Link href="/" className="text-2xl font-bold text-convex-dark-blue">
+          <Link href="/">
             <Image src="/images/logo_dark_blue.svg" alt="Convex Logo" width={200} height={80} className="w-50 h-20" />
           </Link>
 
         </div>
         <div className="flex gap-6">
-          <Link href="/ecosystem" className="text-convex-medium-blue hover:text-convex-dark-blue transition-colors">
+          <Link href="/ecosystem" className="nav-title">
             Ecosystem
           </Link>
           {/*
-          <Link href="/team" className="text-convex-medium-blue hover:text-convex-dark-blue transition-colors">
+          <Link href="/team" className="nav-title">
             Team
           </Link>
           */}
-          <Link href="/tools" className="text-convex-medium-blue hover:text-convex-dark-blue transition-colors">
+          <Link href="/tools" className="nav-title">
             Tools
           </Link>
-          <a href="https://docs.convex.world" target="discord" className="text-convex-medium-blue hover:text-convex-dark-blue transition-colors">
+          <a href="https://docs.convex.world" target="discord" className="nav-title">
             Docs
             <Image src="/link.svg" alt="Link" width={16} height={16} className="inline-block w-4 h-4 ml-1" />
           </a>
-          <a href="https://discord.com/invite/xfYGq4CT7v" target="docs" className="text-convex-medium-blue hover:text-convex-dark-blue transition-colors">
+          <a href="https://discord.com/invite/xfYGq4CT7v" target="docs" className="nav-title">
             Discord
             <Image src="/link.svg" alt="Link" width={16} height={16} className="inline-block w-4 h-4 ml-1" />
           </a>
